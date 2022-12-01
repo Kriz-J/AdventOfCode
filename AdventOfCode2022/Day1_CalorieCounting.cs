7ad51a511b7013f605f1fb2e6f7db706f50aa29a
Kriz-J <119603220+Kriz-J@users.noreply.github.com>
2022-12-01 18:33:46 +0100

    Add Solutions To Puzzles 1 & 2 For December 1st

--- AdventOfCode2022/Day1_CalorieCounting.cs
+++ AdventOfCode2022/Day1_CalorieCounting.cs
@@ -4,11 +4,31 @@ public class Day1_CalorieCounting
 {
     public static void Puzzle1()
     {
+        var inputData = File.ReadAllLines(@"..\..\..\Resources\Day1_Puzzle1_Input.txt");
 
-    }
+        var elvesCalories = new List<int>();
 
-    public static void Puzzle2()
-    {
+        var elfCalories = 0;
         
+        foreach (var row in inputData)
+        {
+
+            if (string.IsNullOrWhiteSpace(row))
+            {
+                elvesCalories.Add(elfCalories);
+                elfCalories = 0;
+                continue;
+            }
+
+            if (!int.TryParse(row, out var itemCalories))
+            {
+                throw new ArgumentException("Error: Cannot process row string to integer.");
+            }
+
+            elfCalories += itemCalories;
+        }
+        
+        Console.WriteLine(@$"The answer to the first puzzle is: {elvesCalories.Max()}");
+        Console.WriteLine(@$"The answer to the second puzzle is: {elvesCalories.OrderByDescending(e => e).Take(3).Sum()}");
     }
 }
