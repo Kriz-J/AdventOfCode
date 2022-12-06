3b4dd5bf9287e846ec7dd53ab06eb5cc592cd284
Kriz-J <119603220+Kriz-J@users.noreply.github.com>
2022-12-06 16:47:52 +0100

    Add Solution For December 6th.

--- AdventOfCode2022/Day6_TuningTrouble.cs
+++ AdventOfCode2022/Day6_TuningTrouble.cs
@@ -1,7 +1,37 @@
-﻿public class Day6_TuningTrouble
+﻿namespace AdventOfCode2022;
+
+public class Day6_TuningTrouble
 {
+    private static readonly string DataStreamBuffer = File.ReadAllText(@"..\..\..\Resources\Day6_Puzzle_Input.txt");
+
+    public static int StartOfPacketMarkerLength => 4;
+    public static int StartOfMessageMarkerLength => 14;
+
     public static void Part1()
     {
+        var firstOccurrence = FindEndOfMarker(DataStreamBuffer, StartOfPacketMarkerLength);
+        Console.WriteLine(@$"The answer to the first puzzle is {firstOccurrence}");
+    }
 
+    public static void Part2()
+    {
+        var firstOccurrence = FindEndOfMarker(DataStreamBuffer, StartOfMessageMarkerLength);
+        Console.WriteLine(@$"The answer to the second puzzle is {firstOccurrence}");
+    }
+
+    private static int FindEndOfMarker(string sequence, int markerLength)
+    {
+        var endOfMarker = markerLength;
+
+        for (int i = markerLength; i <= sequence.Length; i++)
+        {
+            if (sequence[(i - markerLength)..i].Distinct().Count() == markerLength)
+            {
+                endOfMarker = i;
+                break;
+            }
+        }
+
+        return endOfMarker;
     }
 }
