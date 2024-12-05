2285af7c9544720dabe65824c5227fce89af28b8
Kriz-J <119603220+Kriz-J@users.noreply.github.com>
2024-12-05 13:27:03 +0100

    Remove static

--- 2024/Days/Day01_HistorianHysteria.cs
+++ 2024/Days/Day01_HistorianHysteria.cs
@@ -2,9 +2,9 @@
 
 public class Day01_HistorianHysteria
 {
-    public static string[] Input { get; set; } = SantasLittleHelpers.ReadFileRows("2024-12-01.txt");
+    public string[] Input { get; set; } = SantasLittleHelpers.ReadFileRows("2024-12-01.txt");
 
-    public static void Puzzle1()
+    public void Puzzle1()
     {
         var (left, right) = CreateSortedLists();
 
@@ -15,7 +15,7 @@ public class Day01_HistorianHysteria
         Console.WriteLine($"Answer: Total distance between the lists is {totalDistance}.");
     }
 
-    public static void Puzzle2()
+    public void Puzzle2()
     {
         var (left, right) = CreateSortedLists();
 
@@ -26,7 +26,7 @@ public class Day01_HistorianHysteria
         Console.WriteLine($"Answer: Total similarity of the lists is {totalSimilarity}.");
     }
 
-    private static (List<int> Left, List<int> Right) CreateSortedLists()
+    private (List<int> Left, List<int> Right) CreateSortedLists()
     {
         var locationIDs = Input
             .SelectMany(row => row.Split("   "))
