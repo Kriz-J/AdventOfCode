515c3b82f5757a15084641c4a2d323dad2b42468
Kriz-J <119603220+Kriz-J@users.noreply.github.com>
2024-12-08 20:25:44 +0100

    Add Puzzle 2 For Day 3

--- 2024/Days/Day03_MullItOver.cs
+++ 2024/Days/Day03_MullItOver.cs
@@ -5,29 +5,53 @@ namespace Advent2024.Days;
 public class Day03_MullItOver
 {
     public static string Input { get; set; } = SantasLittleHelpers.ReadFile("2024-12-03.txt");
+    private const string FirstFactor = @"(?<=mul\()\d{1,3}(?=,\d{1,3}\))";
+    private const string SecondFactor = @"(?<=mul\(\d{1,3},)\d{1,3}(?=\))";
 
     public static void Puzzle1()
     {
-        const string firstFactor = @"(?<=mul\()\d{1,3}(?=,\d{1,3}\))";
-        const string secondFactor = @"(?<=mul\(\d{1,3},)\d{1,3}(?=\))";
-
-        var regex = new Regex($"{firstFactor}|{secondFactor}");
+        var regex = new Regex($"{FirstFactor}|{SecondFactor}");
         var factors = regex
             .Matches(Input)
             .Select(m => int.Parse(m.Value))
             .ToList();
 
         var mulSum = factors
-            .Select((value, index) => (Value:value, Index:index))
+            .Select((value, index) => (Value: value, Index: index))
             .Where(pair => pair.Index % 2 == 0)
             .Select(pair => pair.Value * factors[pair.Index + 1])
             .Sum();
 
-        Console.WriteLine($"Answer: The sum of all multiplications is {mulSum}"); //183788984
+        Console.WriteLine($"Answer: The sum of all multiplications is {mulSum}");
     }
 
     public static void Puzzle2()
     {
+        var regex = new Regex($@"do\(\)|don't\(\)|{FirstFactor}|{SecondFactor}");
+        var instructions = regex.Matches(Input).Select(m => m.Value).ToList();
 
+        var i = 0;
+        var mulSum = 0;
+        while (i < instructions.Count)
+        {
+            switch (instructions[i])
+            {
+                case "don't()":
+                    while (i < instructions.Count && instructions[i] != "do()")
+                    {
+                        i++;
+                    }
+                    break;
+                case "do()":
+                    i++;
+                    break;
+                default:
+                    mulSum += int.Parse(instructions[i]) * int.Parse(instructions[i + 1]);
+                    i += 2;
+                    break;
+            }
+        }
+
+        Console.WriteLine($"Answer: The sum of all multiplications is {mulSum}"); //62098619
     }
 }
