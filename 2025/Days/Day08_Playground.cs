0d6757351787be60d4bb6d1eeff425e3959dc156
Kriz-J <119603220+Kriz-J@users.noreply.github.com>
2026-01-07 17:21:44 +0100

    Refactor day 8 puzzle 1 and add solution for day 8 puzzle 2.

--- 2025/Days/Day08_Playground.cs
+++ 2025/Days/Day08_Playground.cs
@@ -7,7 +7,7 @@ public static class Day08_Playground
     private static readonly string[] Input = SantasLittleHelpers.ReadFileRows("2025-12-08.txt");
 
     private static readonly Dictionary<(Position P1, Position P2), long> JunctionBoxDistances = [];
-    
+
     private static readonly Dictionary<Position, HashSet<Position>> Circuits = [];
 
     private static readonly Dictionary<Position, Position> Location = [];
@@ -18,79 +18,62 @@ public static class Day08_Playground
 
         FindDistances(junctionBoxPositions);
 
+        var OrderedJunctionBoxDistances = JunctionBoxDistances.OrderBy(kvp => kvp.Value).ToDictionary();
+
         foreach (var position in junctionBoxPositions)
         {
             Circuits.Add(position, [position]);
         }
-        
+
         for (int i = 0; i < junctionBoxPositions.Length; i++)
         {
-            var shortestConnection= JunctionBoxDistances.Where(kvp => kvp.Value > 0).MinBy(kvp => kvp.Value);
+            var shortestConnection = OrderedJunctionBoxDistances.First();
+            OrderedJunctionBoxDistances.Remove(shortestConnection.Key);
 
-            var p1 = shortestConnection.Key.P1;
-            var p2 = shortestConnection.Key.P2;
+            var junctionBox1 = shortestConnection.Key.P1;
+            var junctionBox2 = shortestConnection.Key.P2;
 
-            if (Circuits.ContainsKey(p1) && Circuits.ContainsKey(p2)) // P1 & P2 are both circuit-key (both can contain multiple junction boxes)
-            {
-                foreach (var position in Circuits[p2])
-                {
-                    Circuits[p1].Add(position);
-                    Location[position] = p1;
-                }
-                Circuits.Remove(p2);
-            }
-            else if (Circuits.ContainsKey(p1) && Location.TryGetValue(p2, out var location1)) // P1 is circuit-key and P2 has location
-            {
-                if (p1 == location1)
-                {
-                    JunctionBoxDistances.Remove((p1, p2));
-                    continue;
-                }
-
-                foreach (var position in Circuits[location1])
-                {
-                    Circuits[p1].Add(position);
-                    Location[position] = p1;
-                }
-                Circuits.Remove(location1);
-            }
-            else if (Location.TryGetValue(p1, out var location2) && Circuits.ContainsKey(p2)) // P1 has location and P2 is circuit-key
-            {
-                if (location2 == p2)
-                {
-                    JunctionBoxDistances.Remove((p1, p2));
-                    continue;
-                }
-
-                foreach (var position in Circuits[p2])
-                {
-                    Circuits[location2].Add(position);
-                    Location[position] = location2;
-                }
-                Circuits.Remove(p2);
-            }
-            else if (Location.TryGetValue(p1, out var location3) && Location.TryGetValue(p2, out var location4)) // P1 has location and P2 has location
-            {
-                if (location3 == location4)
-                {
-                    JunctionBoxDistances.Remove((p1, p2));
-                    continue;
-                }
-
-                foreach (var position in Circuits[location4])
-                {
-                    Circuits[location3].Add(position);
-                    Location[position] = location3;
-                }
-                Circuits.Remove(location4);
-            }
-
-            JunctionBoxDistances.Remove((p1, p2)); // Mirror is never added initially
+            GroupJunctionBoxes(junctionBox1, junctionBox2);
         }
 
-        var circuitSize = Circuits.Values.Select(s => s.Count).OrderByDescending(s => s).ToList().Take(3).Aggregate(1, (current, count) => current * count);
-        
+        var circuitSize = Circuits.Values
+            .Select(s => s.Count)
+            .OrderByDescending(s => s)
+            .Take(3)
+            .Aggregate(1, (current, count) => current * count);
+
         Console.WriteLine($"Answer: The three largest circuits produce {circuitSize}.");
+    }
+
+    public static void Puzzle2()
+    {
+        var junctionBoxPositions = ParseJunctionBoxPositions();
+
+        FindDistances(junctionBoxPositions);
+
+        var OrderedJunctionBoxDistances = JunctionBoxDistances.OrderBy(kvp => kvp.Value).ToDictionary();
+
+        foreach (var position in junctionBoxPositions)
+        {
+            Circuits.Add(position, [position]);
+        }
+
+        KeyValuePair<(Position P1, Position P2), long> shortestConnection;
+        do
+        {
+            shortestConnection = OrderedJunctionBoxDistances.First();
+            OrderedJunctionBoxDistances.Remove(shortestConnection.Key);
+
+            var junctionBox1 = shortestConnection.Key.P1;
+            var junctionBox2 = shortestConnection.Key.P2;
+
+            GroupJunctionBoxes(junctionBox1, junctionBox2);
+
+        } while (Circuits.Count > 1);
+
+        var distanceFromWall = (long)shortestConnection.Key.P1.x * shortestConnection.Key.P2.x;
+
+        Console.WriteLine($"Answer: Distance from wall {distanceFromWall}.");
     }
 
     private static Position[] ParseJunctionBoxPositions()
@@ -121,17 +104,52 @@ public static class Day08_Playground
             {
                 var p2 = junctionBoxPositions[j];
 
-                JunctionBoxDistances.TryAdd((p1, p2), SquaredEuclideanDistance(p1, p2));
+                JunctionBoxDistances.Add((p1, p2), SquaredEuclideanDistance(p1, p2)); // Mirror distance is never added
             }
         }
     }
-    
+
     private static long SquaredEuclideanDistance(Position p1, Position p2) // Will work just as well without Math.Sqrt for our usage
     {
         return (long)(p1.x - p2.x) * (p1.x - p2.x) + (long)(p1.y - p2.y) * (p1.y - p2.y) + (long)(p1.z - p2.z) * (p1.z - p2.z);
     }
 
-    public static void Puzzle2()
+    private static void GroupJunctionBoxes(Position p1, Position p2)
     {
+        if (Circuits.ContainsKey(p1) && Circuits.ContainsKey(p2)) // P1 & P2 are both circuit-key (both can contain multiple junction boxes)
+        {
+            MergeCircuits(p1, p2);
+        }
+        else if (Circuits.ContainsKey(p1) && Location.TryGetValue(p2, out var locationOfP2)) // P1 is circuit-key and P2 has location
+        {
+            if (p1 != locationOfP2)
+            {
+                MergeCircuits(p1, locationOfP2);
+            }
+        }
+        else if (Location.TryGetValue(p1, out var locationOfP1) && Circuits.ContainsKey(p2)) // P1 has location and P2 is circuit-key
+        {
+            if (locationOfP1 != p2)
+            {
+                MergeCircuits(locationOfP1, p2);
+            }
+        }
+        else if (Location.TryGetValue(p1, out locationOfP1) && Location.TryGetValue(p2, out locationOfP2)) // P1 has location and P2 has location
+        {
+            if (locationOfP1 != locationOfP2)
+            {
+                MergeCircuits(locationOfP1, locationOfP2);
+            }
+        }
+    }
+
+    private static void MergeCircuits(Position main, Position source)
+    {
+        foreach (var junctionBox in Circuits[source])
+        {
+            Circuits[main].Add(junctionBox);
+            Location[junctionBox] = main;
+        }
+        Circuits.Remove(source);
     }
 }
