e1982a2c5e0eee2bb2adcbeb9439b5779f679045
Kriz-J <119603220+Kriz-J@users.noreply.github.com>
2026-01-13 15:38:46 +0100

    Add solutions for day 9

--- 2025/Days/Day09_MovieTheater.cs
+++ 2025/Days/Day09_MovieTheater.cs
@@ -8,44 +8,18 @@ public static class Day09_MovieTheater
 
     public static void Puzzle1()
     {
-        var coordinates = new Position[Input.Length];
-
-        for (int i = 0; i < Input.Length; i++)
-        {
-            var parts = Input[i].Split(',');
-
-            var x = int.Parse(parts[0]);
-            var y = int.Parse(parts[1]);
-
-            coordinates[i] = new Position(x, y);
-        }
+        var vertices = ParseVertices(Input);
 
         var largestArea = 0L;
-        var area = 0L;
 
-        for (int i = 0; i < coordinates.Length; i++)
+        for (int i = 0; i < vertices.Length; i++)
         {
-            for (int j = i + 1; j < coordinates.Length; j++)
+            for (int j = i + 1; j < vertices.Length; j++)
             {
-                if (IsFirstQuadrant(coordinates[i], coordinates[j]))
-                {
-                    area = (1L + coordinates[j].x - coordinates[i].x) * (1L + coordinates[j].y - coordinates[i].y);
-                }
+                var origin = vertices[i];
+                var oppositeCorner = vertices[j];
 
-                if (IsSecondQuadrant(coordinates[i], coordinates[j]))
-                {
-                    area = (1L + coordinates[i].x - coordinates[j].x) * (1L + coordinates[j].y - coordinates[i].y);
-                }
-
-                if (IsThirdQuadrant(coordinates[i], coordinates[j]))
-                {
-                    area = (1L + coordinates[i].x - coordinates[j].x) * (1L + coordinates[i].y - coordinates[j].y);
-                }
-
-                if (IsFourthQuadrant(coordinates[i], coordinates[j]))
-                {
-                    area = (1L + coordinates[j].x - coordinates[i].x) * (1L + coordinates[i].y - coordinates[j].y);
-                }
+                var area = (1L + Math.Abs(origin.x - oppositeCorner.x)) * (1L + Math.Abs(origin.y - oppositeCorner.y));
 
                 if (area > largestArea)
                 {
@@ -59,85 +33,189 @@ public static class Day09_MovieTheater
 
     public static void Puzzle2()
     {
-        var coordinates = new Position[Input.Length];
-
-        for (int i = 0; i < Input.Length; i++)
-        {
-            var parts = Input[i].Split(',');
-
-            var x = int.Parse(parts[0]);
-            var y = int.Parse(parts[1]);
-
-            coordinates[i] = new Position(x, y);
-        }
-
-        var listCoordinates = coordinates.ToList();
+        var vertices = ParseVertices(Input);
+        var compressedVertices = CompressVertices(vertices);
+        var compressedBorder = ConstructCompressedBorder(compressedVertices);
+        var compressedFill = FillInterior(compressedBorder);
+        var compressedPolygon = new HashSet<Position>(compressedBorder);
+        compressedPolygon.UnionWith(compressedFill);
 
         var largestArea = 0L;
-        var area = 0L;
-        Position ne = (0, 0);
-        Position nw = (0, 0);
-        Position sw = (0, 0);
-        Position se = (0, 0);
-
-        for (int i = 0; i < coordinates.Length; i++)
+        for (var i = 0; i < vertices.Length; i++)
         {
-            for (int j = i + 1; j < coordinates.Length; j++)
+            for (var j = i + 1; j < vertices.Length; j++)
             {
-                if (IsFirstQuadrant(coordinates[i], coordinates[j]))
+                var origin = vertices[i];
+                var oppositeCorner = vertices[j];
+
+                var area = (1L + Math.Abs(origin.x - oppositeCorner.x)) * (1L + Math.Abs(origin.y - oppositeCorner.y));
+
+                var (ne, nw, sw, se) = FindCornersOfRectangle(compressedVertices[i], compressedVertices[j]);
+
+                if (area > largestArea && IsValidRectangle(ne, nw, sw, se, compressedPolygon))
                 {
-                    area = (1L + coordinates[j].x - coordinates[i].x) * (1L + coordinates[i].y - coordinates[j].y);
-
-                    ne = coordinates[j];
-                    nw = new Position(coordinates[i].x, coordinates[j].y);
-                    sw = coordinates[i];
-                    se = new Position(coordinates[j].x, coordinates[i].y);
-                }
-
-                else if (IsSecondQuadrant(coordinates[i], coordinates[j]))
-                {
-                    area = (1L + coordinates[i].x - coordinates[j].x) * (1L + coordinates[i].y - coordinates[j].y);
-
-                    ne = new Position(coordinates[i].x, coordinates[j].y);
-                    nw = coordinates[j];
-                    sw = new Position(coordinates[j].x, coordinates[i].y);
-                    se = coordinates[i];
-                }
-
-                else if (IsThirdQuadrant(coordinates[i], coordinates[j]))
-                {
-                    area = (1L + coordinates[i].x - coordinates[j].x) * (1L + coordinates[j].y - coordinates[i].y);
-
-                    ne = coordinates[i];
-                    nw = new Position(coordinates[j].x, coordinates[i].y);
-                    sw = coordinates[j];
-                    se = new Position(coordinates[i].x, coordinates[j].y);
-                }
-
-                else if (IsFourthQuadrant(coordinates[i], coordinates[j]))
-                {
-                    area = (1L + coordinates[j].x - coordinates[i].x) * (1L + coordinates[j].y - coordinates[i].y);
-
-                    ne = new Position(coordinates[j].x, coordinates[i].y);
-                    nw = coordinates[i];
-                    sw = new Position(coordinates[i].x, coordinates[j].y);
-                    se = coordinates[j];
-                }
-
-                if (listCoordinates.Any(c => c.x >= ne.x && c.y <= ne.y) &&
-                    listCoordinates.Any(c => c.x <= nw.x && c.y <= nw.y) &&
-                    listCoordinates.Any(c => c.x <= sw.x && c.y >= sw.y) &&
-                    listCoordinates.Any(c => c.x >= se.x && c.y >= se.y))
-                {
-                    if (area > largestArea)
-                    {
-                        largestArea = area;
-                    }
+                    largestArea = area;
                 }
             }
         }
 
         Console.WriteLine($"Answer: The largest area is {largestArea}.");
+    }
+
+    private static Position[] ParseVertices(string[] input)
+    {
+        var vertices = new Position[input.Length];
+
+        for (var i = 0; i < input.Length; i++)
+        {
+            var parts = input[i].Split(',');
+
+            var x = int.Parse(parts[0]);
+            var y = int.Parse(parts[1]);
+
+            vertices[i] = new Position(x, y);
+        }
+
+        return vertices;
+    }
+
+    private static Position[] CompressVertices(Position[] vertices)
+    {
+        var uniqueX = vertices.Select(p => p.x).Distinct().Order().ToArray();
+        var uniqueY = vertices.Select(p => p.y).Distinct().Order().ToArray();
+
+        var compressedX = new Dictionary<int, int>();
+        for (var i = 0; i < uniqueX.Length; i++)
+        {
+            compressedX[uniqueX[i]] = i;
+        }
+
+        var compressedY = new Dictionary<int, int>();
+        for (var i = 0; i < uniqueY.Length; i++)
+        {
+            compressedY[uniqueY[i]] = i;
+        }
+
+        var compressedVertices = new Position[vertices.Length];
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            compressedVertices[i] = new Position(compressedX[vertices[i].x], compressedY[vertices[i].y]);
+        }
+
+        return compressedVertices;
+    }
+
+    private static HashSet<Position> ConstructCompressedBorder(Position[] compressedVertices)
+    {
+        var compressedBorder = new HashSet<Position>();
+
+        var inputLength = compressedVertices.Length;
+        for (var i = 0; i < inputLength; i++)
+        {
+            var current = compressedVertices[i];
+            var next = compressedVertices[(i + 1) % inputLength];
+
+            var dx = next.x - current.x;
+            var dy = next.y - current.y;
+
+            var xCoordinates = dx > 0 ? Enumerable.Range(current.x, 1 + dx) : Enumerable.Range(next.x, 1 + Math.Abs(dx)).Reverse(); // right:left
+            var yCoordinates = dy > 0 ? Enumerable.Range(current.y, 1 + dy) : Enumerable.Range(next.y, 1 + Math.Abs(dy)).Reverse(); // down:up
+
+            var borderPositionsX = xCoordinates.Select(x => new Position(x, current.y)).ToHashSet();
+            var borderPositionsY = yCoordinates.Select(y => new Position(current.x, y)).ToHashSet();
+
+            compressedBorder.UnionWith(borderPositionsX);
+            compressedBorder.UnionWith(borderPositionsY);
+        }
+
+        return compressedBorder;
+    }
+
+    private static HashSet<Position> FillInterior(HashSet<Position> border) // Alternative to flood fill which doesn't require a known point inside - Shoots rays to center
+    {
+        var fill = new HashSet<Position>();
+
+        var borderList = border.ToArray();
+        var borderListLength = borderList.Length;
+
+        for (var i = 0; i < borderListLength; i++)
+        {
+            var current = borderList[i];
+            var next = borderList[(i + 1) % borderListLength];
+
+            var dx = next.x - current.x;
+            var dy = next.y - current.y;
+
+            if (dx == 0)
+            {
+                var direction = dy > 0 ? -1 : 1; // right:left
+
+                while (true)
+                {
+                    current.x += direction;
+                    if (border.Contains(current))
+                    {
+                        break;
+                    }
+                    fill.Add(current);
+                }
+            }
+            else if (dy == 0)
+            {
+                var direction = dx > 0 ? 1 : -1; // down:up
+
+                while (true)
+                {
+                    current.y += direction;
+                    if (border.Contains(current))
+                    {
+                        break;
+                    }
+                    fill.Add(current);
+                }
+            }
+        }
+
+        return fill;
+    }
+
+    private static (Position NorthEast, Position NorthWest, Position SouthWest, Position SouthEast) FindCornersOfRectangle(Position origin, Position oppositeCorner)
+    {
+        Position ne = (0, 0);
+        Position nw = (0, 0);
+        Position sw = (0, 0);
+        Position se = (0, 0);
+
+        if (IsFirstQuadrant(origin, oppositeCorner))
+        {
+            ne = oppositeCorner;
+            nw = new Position(origin.x, oppositeCorner.y);
+            sw = origin;
+            se = new Position(oppositeCorner.x, origin.y);
+        }
+        else if (IsSecondQuadrant(origin, oppositeCorner))
+        {
+            ne = new Position(origin.x, oppositeCorner.y);
+            nw = oppositeCorner;
+            sw = new Position(oppositeCorner.x, origin.y);
+            se = origin;
+        }
+        else if (IsThirdQuadrant(origin, oppositeCorner))
+        {
+            ne = origin;
+            nw = new Position(oppositeCorner.x, origin.y);
+            sw = oppositeCorner;
+            se = new Position(origin.x, oppositeCorner.y);
+        }
+        else if (IsFourthQuadrant(origin, oppositeCorner))
+        {
+            ne = new Position(oppositeCorner.x, origin.y);
+            nw = origin;
+            sw = new Position(origin.x, oppositeCorner.y);
+            se = oppositeCorner;
+        }
+
+        return (NorthEast: ne, NorthWest: nw, SouthWest: sw, SouthEast: se);
     }
 
     private static bool IsFirstQuadrant(Position origin, Position position) => origin.x <= position.x && origin.y >= position.y;
@@ -147,7 +225,29 @@ public static class Day09_MovieTheater
     private static bool IsThirdQuadrant(Position origin, Position position) => origin.x >= position.x && origin.y <= position.y;
 
     private static bool IsFourthQuadrant(Position origin, Position position) => origin.x <= position.x && origin.y <= position.y;
-}
 
-//4 584 201 744 HIGH
-//4 629 504 600 HIGH
+    private static bool IsValidRectangle(Position ne, Position nw, Position sw, Position se, HashSet<Position> polygon)
+    {
+        return IsAllPointsOnEdgeInPolygon(ne, nw, -1, 0, polygon) &&
+               IsAllPointsOnEdgeInPolygon(nw, sw, 0, 1, polygon) &&
+               IsAllPointsOnEdgeInPolygon(sw, se, 1, 0, polygon) &&
+               IsAllPointsOnEdgeInPolygon(se, ne, 0, -1, polygon);
+    }
+
+    private static bool IsAllPointsOnEdgeInPolygon(Position start, Position end, int dx, int dy, HashSet<Position> polygon)
+    {
+        var current = start;
+
+        while (current != end)
+        {
+            if (!polygon.Contains(current))
+            {
+                return false;
+            }
+
+            current = new Position(current.x + dx, current.y + dy);
+        }
+
+        return true;
+    }
+}
